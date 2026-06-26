2eba2bdee652ec69e484dda34aa2d2a31e7e68e9
Lennart <lenny.hock@outlook.de>
2026-06-26 02:04:52 +0200

    Cleanup + Translation Fix

--- app/src/main/java/com/foodscanner/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/foodscanner/ui/screens/ProductScreen.kt
@@ -1,58 +1,29 @@
 package com.foodscanner.ui.screens
 
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.foodscanner.R
 import com.foodscanner.ui.components.productscreen.HealthScoreCard
 import com.foodscanner.ui.components.productscreen.ProductImage
 import com.foodscanner.ui.components.productscreen.ProductName
 import com.foodscanner.ui.components.utility.VitalScanFooter
 import com.foodscanner.ui.components.utility.VitalScanHeader
-import com.foodscanner.ui.components.utility.customShadow
 import com.foodscanner.ui.theme.FoodScannerTheme
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.stringResource
 import com.foodscanner.ui.components.productscreen.AllergenAlert
 import com.foodscanner.ui.components.productscreen.IngredientList
 import com.foodscanner.ui.components.productscreen.NutrimentCircles
-import com.foodscanner.ui.components.productscreen.NutrimentLegendRow
-import kotlin.math.roundToInt
 
 @Composable
 fun ProductScreen() {
